6d5f883fc7e124a757287f4ca10e4467ed1851c7
user <dover0601@naver.com>
2022-01-10 16:18:24 +0900

    JPA에서 DB 관리 로그 Class생성

--- src/main/java/com/shop/test/Application.java
+++ src/main/java/com/shop/test/Application.java
@@ -6,12 +6,15 @@ package com.shop.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args); // 내장 WAS를 실행한다.
         // 스프링 부트에서는 내장 WAS를 사용하는것을 권장하고 있다.
         // '언제 어디서나 같은 환경에서 스프링 부트를 배포'할 수 있기 때문이다.
+
     }
 }
---
+++ src/main/java/com/shop/test/web/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.shop.test.web.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+//모든 Entity의 상위 클래스가 되어 Entity 들의 변경사항을 기록해주는 로그를 자동으로 관리하는 역할
+@Getter
+@MappedSuperclass   //JPA Entity클래스들이 이 클래스를 상속할때 필드들도 컬럼으로 인식하게 해준다.
+@EntityListeners(AuditingEntityListener.class)  //Auditing기능을 포함시킨다.
+public abstract class BaseTimeEntity {
+
+    @CreatedDate    //Entity가 생성될때마다 시간을 자동으로 저장해준다.
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate   //Entity값이 변경될때마다 시간을 자동으로 저장해준다.
+    private LocalDateTime modifiedDate;
+
+}
