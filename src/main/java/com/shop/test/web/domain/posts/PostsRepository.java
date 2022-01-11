428842354739da52cdc420e9e4199fb9eb337ac6
user <dover0601@naver.com>
2022-01-11 14:24:44 +0900

    게시글 등록, 수정

--- src/main/java/com/shop/test/web/domain/posts/PostsRepository.java
+++ src/main/java/com/shop/test/web/domain/posts/PostsRepository.java
@@ -1,8 +1,17 @@
 package com.shop.test.web.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //JpaRepository<Entity 클래스, PK 타입>을 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    //JPA에서도 직접 쿼리로 데이터를 가져올 수 있다.
+    //SELECT정도의 조회문은 JPA로도 조회할 수 있지만
+    //조인이나 조건이 복잡한 쿼리들은 조회가 어렵기 때문에
+    //Mybatis나 querydsl같은 프레임워크를 조회용으로 JPA와 함꼐 사용한다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/shop/test/web/service/posts/PostsService.java
+++ src/main/java/com/shop/test/web/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.shop.test.web.service.posts;
 
 import com.shop.test.web.domain.posts.Posts;
 import com.shop.test.web.domain.posts.PostsRepository;
+import com.shop.test.web.dto.PostsListResponseDto;
 import com.shop.test.web.dto.PostsResponseDto;
 import com.shop.test.web.dto.PostsSaveRequestDto;
 import com.shop.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor    //final이 선언된 모든 필드를 인자값으로 하는 생성자를 롬복이 생성해줌
 @Service
@@ -34,4 +37,12 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly=true)   // * readOnly 옵션은 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //.stream().map -> .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
 }
